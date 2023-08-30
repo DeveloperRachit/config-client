bb7137c00971a62f2f1198cb7bf093b49936f6a4
DeveloperRachit <kumar1002@outlook.com>
2023-08-30 18:04:33 +0530

    adding server

---
+++ src/main/java/com/configclient/implement/SloConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.configclient.implement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SloConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SloConfigClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/configclient/implement/SloConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configclient.implement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SloConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
